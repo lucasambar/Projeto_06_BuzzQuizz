7ce9018e5a2d79ec656455339e47f968d96ac68d
duuda2011@gmail.com <elissonalvessilva@MacBook-Pro-de-Elisson.local>
2022-09-01 17:05:02 -0300

    renderizado os quizzes no site

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
+/*
 // Gustavo
-
 function pegarperguntasdeumquiz(){
     const promessa = axios.get("https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes/ID_DO_QUIZZ");
     promessa.then(exibirperguntasdeumquiz);
-    /* Fazer catch */
+    /*Fazer catch*
     promessa.catch();
 }
 
@@ -51,5 +51,45 @@ function respostasaleatorias(pergunta){
     pergunta.answers.sort(comparador);
     return pergunta;
 }
-
 // Gustavo
+*/
+
+// Duda // 
+
+// etapa 2: preciso pegar as receitas no servidor ( enviar a cartinha ) 
+
+let quizzes= [];
+const dados= axios.get('https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes');
+ dados.then(renderizarQuizzes);
+
+// etapa 3: receber a resposta (cartinha) do servidor 
+
+function responseQuizzes (response) {
+    console.log(response);
+    quizzes=response.data;
+}
+
+// etapa 4: processar a resposta e mostrar na tela ( renderizar ) as receitas
+const ul = document.querySelector('.quizzes-cards');
+
+function renderizarQuizzes(response){
+    const quizzes=response.data;
+    ul.innerHTML= '';
+    let html = '';
+    
+    for ( let i= 0; i < quizzes.length; i++){
+         html += `
+        <li class="quizz" quizz-id=${quizzes[i].id}>
+            <figure class="image-quizz">
+                <img src=${quizzes[i].image} />
+                <figcaption>
+                    <h3 class="title">
+                        ${quizzes[i].title}
+                    </h3>
+                </figcaption>
+            </figure>
+        </li>
+        `
+    }
+    ul.innerHTML = html;
+}
