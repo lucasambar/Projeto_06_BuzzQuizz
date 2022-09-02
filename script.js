5f78f41939709697fbe56a462cd3855c62e6d2d2
Cherrying55 <cherrying556@protonmail.com>
2022-09-02 13:19:23 -0300

    Gustavo: Exibição de um quiz quase concluido

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 
 // Gustavo
+let counterdeacertos = 0;
+
 function pegarperguntasdeumquiz(){
     const promessa = axios.get("https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes/ID_DO_QUIZZ");
     promessa.then(exibirperguntasdeumquiz);
@@ -9,8 +11,8 @@ function pegarperguntasdeumquiz(){
 
 function exibirperguntasdeumquiz(r){
     const quiz = r.data;
-    const paginadoquiz = document.createElement('div');
-    paginadoquiz.classList.add("paginadoquiz");
+    const paginadoquiz = document.querySelector('.paginadoquiz');
+    paginadoquiz.classList.remove("hidden");
     paginadoquiz.innerHTML = `<figure class="bannerdoquiz" style="background: linear-gradient(0deg, rgba(0, 0, 0, 0.57), rgba(0, 0, 0, 0.57)), url("${quiz.image}");">
     <figcaption>
         <h3 class="titulobanner">${quiz.title}</h3>
@@ -21,16 +23,16 @@ function exibirperguntasdeumquiz(r){
     perguntas.sort(comparador);
     perguntas.map(respostasaleatorias);
     let perguntasinnerhtml = "";
-    let j = 0;
+    let perguntaslen = perguntas.length;
     for(let j = 0; j < perguntas.length; j++){
         perguntasinnerhtml += `<div class="perguntadoquiz">
-                                   <div class="titulodapergunta">
+                                   <div class="titulodapergunta" style="background-color:${perguntas[j].color};>
                                         ${perguntas[j].title}
                                    </div>
-                                   <div class="containerdeopçoes">`;
+                                   <div class="containerdeopçoes container${j}">`;
         for(let i = 0; i <perguntas[j].answers.length; i++){
             perguntasinnerhtml += `
-                    <div class="opçao">
+                    <div class="opçao" onclick="escolhadeopçoes(this,${j}, ${perguntaslen})" data-true="${perguntas[j].answers[i].isCorrectAnswer}">
                         <img src="${perguntas[j].answers[i].image}">
                         <h4>
                            ${perguntas[j].answers[i].text}
@@ -40,7 +42,22 @@ function exibirperguntasdeumquiz(r){
         }
         perguntasinnerhtml += `</div></div>`
     }
-
+    // loop aqui
+    for(let a = 0; a < quiz.levels.length;a++){
+        perguntasinnerhtml += `<div class="level ${"level" + a} hidden data-minvalue="${quiz.levels[a].minValue}">
+    <div class="textodefinalizaçao">
+        ${quiz.levels[a].title}
+    </div>
+    <img class="imagemdefinalizaçao" src="${quiz.levels[a].image}">
+    <div class="conclusaofinalizaçao">
+        ${quiz.levels[a].text}
+    </div>
+</div>
+    `;
+    };
+    perguntasinnerhtml += `<button class="reiniciarquiz hidden" onclick="reiniciarquiz()">Reiniciar Quiz</button>
+    <a class="voltarparahome hidden" onclick="voltarparahome(this)">Voltar para Home</a>
+    `;
 }
 
 function comparador() { 
@@ -50,6 +67,66 @@ function comparador() {
 function respostasaleatorias(pergunta){
     pergunta.answers.sort(comparador);
     return pergunta;
+}
+
+function escolhadeopçoes(t, numerodocontainer, length){
+   let selecionado = document.querySelector('.opçaoescolhida');
+   let container = document.querySelector('.container' + numerodocontainer);
+   if(selecionado != null){}
+   else{
+    selecionado = t;
+    selecionado.classList.add("opçaoescolhida");
+    let container2 = Array.from(container.children).map((elemento) => {elemento.classList.add("opaco");return elemento;});
+    selecionado.classList.remove("opaco");
+    if(selecionado.dataset.true === "true"){
+        selecionado.classList.add("opçaocorreta");
+        counterdeacertos++;
+    }
+    else{
+        selecionado.classList.add("opçaoerrada");
+    };
+    const timeout = setTimeout(scrollarparaaproxima, 2000, numerodocontainer);
+   };
+
+   function scrollarparaaproxima(numerodocontainer){
+    let proxima = document.querySelector('.container' + (numerodocontainer + 1));
+    let resultado = parseInt(counterdeacertos / length * 100);
+    if(proxima != null){
+        proxima.scrollIntoView();
+    }
+    else{
+        let definirproxima = document.querySelectorAll('.level');
+        for(const i of definirproxima){
+            if(resultado >= i.dataset.minvalue){
+                /* funciona porque estao em ordem crescente */
+                proxima = i;
+            }
+        }
+        proxima.classList.remove("hidden");
+        proxima.scrollIntoView();
+        proxima.firstChild.textContent = `${resultado}% de acerto: ` + proxima.firstChild.textContent;
+        /*aqui vem o calculo provavelmente, e precisa selecionar o level corretos*/
+        counterdeacertos = 0;
+    }
+   }
+}
+
+function reiniciarquiz(){
+    let paginadoquiz = document.querySelector('.paginadoquiz');
+    paginadoquiz.innerHTML = "";
+    paginadoquiz.classList.add("hidden");
+    const header = document.querySelector(".header");
+    header.scrollIntoView();
+    pegarperguntasdeumquiz();
+}
+
+function voltarparahome(){
+    let paginadoquiz = document.querySelector('.paginadoquiz');
+    paginadoquiz.innerHTML = "";
+    paginadoquiz.classList.add("hidden");
+    const header = document.querySelector(".header");
+    header.scrollIntoView();
+
 }
 // Gustavo
 
